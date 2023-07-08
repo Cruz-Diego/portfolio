d41fc86b82ca45dc461da2ce2d148ac24fbbe6f5
diegocruz <six-cruz@hotmail.com>
2023-07-07 21:50:13 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+/*hamburguer button*/ 
+function deployMenu() {
+    let navbar = document.querySelector('#navbar');
+    let hamburgerBotton = document.querySelector('#Hamburger');
+    hamburgerBotton.addEventListener('click', () => {
+        navbar.classList.toggle('open');
+    });
+};
+deployMenu();
+
+/*welcome button*/
+function scrollDown () {
+    let welcome_button = document.querySelector('#welcome_button');
+    welcome_button.addEventListener('click', () => {
+        window.scroll({
+            top: 1635,
+            behavior: 'smooth'
+          });
+    });
+};
+scrollDown();
+
+/*data visible*/
+function dataVisible() {
+    let data_diego = document.querySelector('#data_diego');
+    let welcome_button = document.querySelector('#welcome_button');
+    welcome_button.addEventListener('click', () => {
+        data_diego.classList.add('data_visible');
+    });
+};
+dataVisible();
+
+/*fixed*/
+function fixedMenu(){
+
+    let navbar = document.querySelector('#navbar');
+    let main = document.querySelector('main');
+    let navbarHeight = navbar.getBoundingClientRect().height;
+    let breakpoint = main.offsetTop - navbarHeight;
+    let windowPos;
+    let isFixed = false;
+
+    function updatePos() {
+        windowPos = window.scrollY;
+    }
+
+    function onScroll() {
+
+        updatePos();
+    
+        if (windowPos >= breakpoint && !isFixed) {
+            navbar.classList.remove('open');
+
+            navbar.classList.add('navbar-fixed');
+            main.style.cssText = "margin-top: " + navbarHeight + 'px;';
+            isFixed = true;
+    
+        } else if (windowPos < breakpoint && isFixed) {
+            navbar.classList.remove('navbar-fixed');
+            main.style.cssText = "margin-top: " + 0;
+            isFixed = false;
+        }
+    }
+
+    document.addEventListener('scroll', onScroll);
+};
+fixedMenu();
